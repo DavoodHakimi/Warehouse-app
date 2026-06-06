961aca576f623cf8eeccb64154aca3efbb6d5891
Davood Hakimi <hakimi.davood@gmail.com>
2026-06-06 23:51:35 +0330

    changed based on new ways of handling each api endpoint

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"github.com/DavoodHakimi/warehouse-app/internal/auth"
+	"github.com/DavoodHakimi/warehouse-app/internal/database"
 	"github.com/DavoodHakimi/warehouse-app/internal/middleware"
+	"github.com/DavoodHakimi/warehouse-app/internal/users"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +12,16 @@ import (
 func Setup() *gin.Engine {
 	routerEng := gin.Default()
 	v1 := routerEng.Group("/api/v1")
+	db := database.GetDB()
 	{
 		authGroup := v1.Group("/auth")
 		{
-			authGroup.POST("/login", auth.LogInHandler)
-			authGroup.POST("/signup", auth.SignUpHandler)
+			authRepo := auth.NewRepository(db)
+			authService := auth.NewService(authRepo)
+			authHandler := auth.NewHandler(authService)
+
+			authGroup.POST("/login", authHandler.LogInHandler)
+			authGroup.POST("/signup", authHandler.SignUpHandler)
 		}
 
 		protected := v1.Group("")
@@ -24,12 +31,16 @@ func Setup() *gin.Engine {
 			protected.GET("/auth/me", auth.MeHandler)
 
 			usersGroup := protected.Group("/users")
+
+			userRepo := users.NewRepository(db)
+			userService := users.NewService(userRepo)
+			userHandler := users.NewHandler(userService)
 			{
-				usersGroup.GET("/")
-				usersGroup.GET("/:userID")
-				usersGroup.POST("")
-				usersGroup.PATCH("/:userID")
-				usersGroup.DELETE("/:userID")
+				usersGroup.GET("/", userHandler.AllUsersHandler)             //returning list of company users, only ceo can see
+				usersGroup.GET("/:userID", userHandler.UserInfoHandler)      //returning user details, only ceo can see
+				usersGroup.POST("", userHandler.UserCreationHandler)         //creating new user, only ceo can do
+				usersGroup.PATCH("/:userID", userHandler.UserUpdateHandler)  //only ceo and manager can edit user info, but not password
+				usersGroup.DELETE("/:userID", userHandler.UserDeleteHandler) //only ceo can delete user
 			}
 
 			ordersGroup := protected.Group("/orders")
