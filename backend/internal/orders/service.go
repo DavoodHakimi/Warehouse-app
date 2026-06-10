7842418443992ab2ef81a2578244345c750dac8a
Davood Hakimi <hakimi.davood.80@gmail.com>
2026-06-10 19:00:14 +0330

    init service, just adding functions

--- backend/internal/orders/service.go
+++ backend/internal/orders/service.go
@@ -7,3 +7,39 @@ type Service struct {
 func NewService(repo *Repository) *Service {
 	return &Service{repo: repo}
 }
+
+func (s *Service) AllOrders(cID int) (*OrdersInfo, error) {
+	return nil, nil
+}
+
+func (s *Service) ReadOrder(orderID string) (*OrderInfoResponse, error) {
+	return nil, nil
+}
+
+func (s *Service) CreateOrder(o *CreateOrderRequest, cid int) error {
+	return nil
+}
+
+func (s *Service) UpdateOrder(o *UpdateOrderRequest, userRequestedID int) error {
+	return nil
+}
+
+func (s *Service) DeleteOrder(oID int) error {
+	return nil
+}
+
+func (s *Service) Approve() {
+
+}
+
+func (s *Service) Pack() {
+
+}
+
+func (s *Service) Ship() {
+
+}
+
+func (s *Service) Receive() {
+
+}
