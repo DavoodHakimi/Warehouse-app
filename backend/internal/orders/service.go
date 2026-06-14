a41f5d7b04fd750166754ee431e3fcb945f6a61a
Davood Hakimi <hakimi.davood.80@gmail.com>
2026-06-14 16:22:55 +0330

    completed order actions

--- backend/internal/orders/service.go
+++ backend/internal/orders/service.go
@@ -1,5 +1,14 @@
 package orders
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/DavoodHakimi/warehouse-app/internal/audit"
+)
+
 type Service struct {
 	repo *Repository
 }
@@ -9,37 +18,185 @@ func NewService(repo *Repository) *Service {
 }
 
 func (s *Service) AllOrders(cID int) (*OrdersInfo, error) {
-	return nil, nil
+	orders, err := s.repo.ReadCompanyOrders(cID)
+	allOrders := OrdersInfo{orders: make([]OrderInfoResponse, 0, len(orders))}
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range orders {
+		allOrders.orders = append(allOrders.orders, OrderInfoResponse{
+			OrderType:           item.OrderType,
+			OrderNumber:         item.OrderNumber,
+			Status:              item.Status,
+			BusinessPartnerName: item.BusinessPartner.Name,
+			Currency:            item.Currency.Name,
+			ExchangeRate:        item.ExchangeRate,
+		})
+	}
+	return &allOrders, nil
 }
 
 func (s *Service) ReadOrder(orderID string) (*OrderInfoResponse, error) {
-	return nil, nil
+	order, _, err := s.CheckOrderExist(orderID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrderInfoResponse{
+		OrderType:           order.OrderType,
+		OrderNumber:         order.OrderNumber,
+		Status:              order.Status,
+		BusinessPartnerName: order.BusinessPartner.Name,
+		Currency:            order.Currency.Name,
+		ExchangeRate:        order.ExchangeRate}, nil
 }
 
 func (s *Service) CreateOrder(o *CreateOrderRequest, cid int) error {
-	return nil
+	allItems := make([]OrderItem, 0, len(o.OrderItems))
+	for _, item := range o.OrderItems {
+		allItems = append(allItems, OrderItem{
+			ProductID:    item.ProductID,
+			Quantity:     item.Quantity,
+			PerItemPrice: item.PerItemPrice,
+		})
+	}
+	order := Order{
+		OrderType:         o.OrderType,
+		OrderNumber:       fmt.Sprintf("ORD-%d", time.Now().UnixNano()),
+		CompanyID:         uint(cid),
+		Status:            "Pending",
+		BusinessPartnerID: o.BusinessPartnerID,
+		CurrencyID:        o.CurrencyID,
+		ExchangeRate:      o.ExchangeRate,
+		OrderItems:        allItems,
+	}
+	return s.repo.Create(&order)
 }
 
 func (s *Service) UpdateOrder(o *UpdateOrderRequest, userRequestedID int) error {
+	changedFields := s.modifiedFields(o)
+	if len(changedFields) == 0 {
+		return errors.New("no changes detected")
+	}
+
+	order := Order{
+		OrderType:         o.OrderType,
+		BusinessPartnerID: o.BusinessPartnerID,
+		CurrencyID:        o.CurrencyID,
+		ExchangeRate:      o.ExchangeRate,
+	}
+	err := s.repo.Update(&order)
+	if err != nil {
+		return err
+	}
+	for field, values := range changedFields {
+		log := audit.Log{
+			EntityType: "product",
+			EntityID:   uint(o.ID),
+			Event:      "updated",
+			Field:      field,
+			OldValue:   values[0],
+			NewValue:   values[1],
+			ByUserID:   uint(userRequestedID),
+		}
+		audit.Record(s.repo.db, &log)
+	}
 	return nil
 }
+func (s *Service) modifiedFields(o *UpdateOrderRequest) map[string][2]string {
+	oldValues, err := s.repo.FindByID(o.ID)
+	if err != nil {
+		return nil
+	}
 
-func (s *Service) DeleteOrder(oID int) error {
-	return nil
+	changes := make(map[string][2]string)
+
+	if o.OrderType != "" && o.OrderType != oldValues.OrderType {
+		changes["OrderType"] = [2]string{oldValues.OrderType, o.OrderType}
+	}
+	if o.BusinessPartnerID != oldValues.BusinessPartnerID {
+		changes["BusinessPartnerID"] = [2]string{strconv.Itoa(int(oldValues.BusinessPartnerID)), strconv.Itoa(int(o.BusinessPartnerID))}
+	}
+	if o.CurrencyID != oldValues.CurrencyID {
+		changes["CurrencyID"] = [2]string{strconv.Itoa(int(oldValues.CurrencyID)), strconv.Itoa(int(o.CurrencyID))}
+	}
+	if o.ExchangeRate != oldValues.ExchangeRate {
+		changes["ExchangeRate"] = [2]string{strconv.FormatFloat(oldValues.ExchangeRate, 'f', -1, 64), strconv.FormatFloat(o.ExchangeRate, 'f', -1, 64)}
+	}
+	return changes
 }
 
-func (s *Service) Approve() {
+func (s *Service) DeleteOrder(orderID string) error {
+	val, err := strconv.Atoi(orderID)
+	if err != nil {
+		return errors.New("Invalid Order ID")
+	}
 
+	order, err := s.repo.FindByID(uint(val))
+	if err != nil {
+		return errors.New("Order Not found")
+	}
+
+	return s.repo.Delete(order)
 }
 
-func (s *Service) Pack() {
+func (s *Service) Approve(orderID string) error {
+	order, val, err := s.CheckOrderExist(orderID)
+	if err != nil {
+		return err
+	}
 
+	if order.Status != "Pending" {
+		return errors.New("This Order status can not changed to approve.")
+	}
+	return s.repo.StatusUpdate(uint(val), "Approved")
 }
 
-func (s *Service) Ship() {
+func (s *Service) Pack(orderID string) error {
+	order, val, err := s.CheckOrderExist(orderID)
+	if err != nil {
+		return err
+	}
 
+	if order.Status != "Approved" || order.OrderType != "Sale" {
+		return errors.New("This Order status can not changed to Packed.")
+	}
+	return s.repo.StatusUpdate(uint(val), "Packed")
 }
 
-func (s *Service) Receive() {
+func (s *Service) Ship(orderID string) error {
+	order, val, err := s.CheckOrderExist(orderID)
+	if err != nil {
+		return err
+	}
 
+	if order.Status != "Packed" || order.OrderType != "Sale" {
+		return errors.New("This Order status can not changed to Shipped.")
+	}
+	return s.repo.StatusUpdate(uint(val), "Shipped")
+}
+
+func (s *Service) Receive(orderID string) error {
+	order, val, err := s.CheckOrderExist(orderID)
+	if err != nil {
+		return err
+	}
+
+	if order.Status != "Approved" || order.OrderType != "purchase" {
+		return errors.New("This Order status can not changed to Approved.")
+	}
+	return s.repo.StatusUpdate(uint(val), "Received")
+}
+
+func (s *Service) CheckOrderExist(orderID string) (*Order, int, error) {
+	val, err := strconv.Atoi(orderID)
+	if err != nil {
+		return nil, val, errors.New("Invalid Order ID")
+	}
+	order, err := s.repo.FindByID(uint(val))
+	if err != nil {
+		return nil, val, err
+	}
+	return order, val, nil
 }
