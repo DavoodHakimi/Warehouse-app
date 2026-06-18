19260e75bf56f0ef5633b2d68ed75eb663973b47
Davood <hakimi.davood.80@gmail.com>
2026-06-18 22:35:19 +0330

    added logging to stdout and file via slog/log

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"log"
+	"log/slog"
+	"os"
 
 	"github.com/DavoodHakimi/warehouse-app/internal/database"
 	"github.com/DavoodHakimi/warehouse-app/internal/database/seed"
@@ -10,19 +11,29 @@ import (
 )
 
 func main() {
+	file, _ := os.OpenFile("../../app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logger := slog.New(slog.NewMultiHandler(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		slog.NewJSONHandler(file, &slog.HandlerOptions{Level: slog.LevelInfo}),
+	))
+
+	slog.SetDefault(logger)
 
 	db := database.GetDB()
 
 	if err := database.RunMigrations(db); err != nil {
-		log.Fatalf("Migration failed: %v", err)
+		slog.Error("migration failed", "error", err)
+		os.Exit(1)
 	}
-	log.Println("Connected to database successfully!")
+	slog.Info("connected to database successfully")
 
 	seeder := seed.NewSeeder(db)
 	if err := seeder.Run(context.Background()); err != nil {
-		log.Fatalf("seed failed: %v", err)
+		slog.Error("seeding operation failed", "error", err)
+		os.Exit(1)
 	}
 
 	r := router.Setup(db)
+	slog.Info("server starting", "port", ":8080")
 	r.Run(":8080")
 }
