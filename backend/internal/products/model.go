4a52bc123c6864fdf8de2c0aee3dfe2434eb06ac
Davood Hakimi <hakimi.davood.80@gmail.com>
2026-06-21 16:58:20 +0330

    updated gorm tags and added check for stock values

--- backend/internal/products/model.go
+++ backend/internal/products/model.go
@@ -14,7 +14,7 @@ type Product struct {
 	ProductNumber string  `gorm:"unique; not null" json:"product_number"`
 	CompanyID     uint    `json:"company_id"`
 	IsFrozen      bool    `gorm:"default:false" json:"is_frozen"`
-	DefaultPrice  float64 `gorm:"type:decimal(10,2)" json:"default_price"`
+	DefaultPrice  float64 `gorm:"type:decimal(25,2)" json:"default_price"`
 
 	// Relationships
 	Company company.Company `gorm:"foreignKey:CompanyID" json:"company,omitempty"`
@@ -23,8 +23,8 @@ type Product struct {
 type Stock struct {
 	gorm.Model
 	ProductID      uint `json:"product_id"`
-	AvailableStock int  `gorm:"not null;default:0" json:"available_stock"`
-	ReservedStock  int  `gorm:"not null;default:0" json:"reserved_stock"`
+	AvailableStock int  `gorm:"not null;default:0;check:available_stock >= 0" binding:"gte=0" json:"available_stock"`
+	ReservedStock  int  `gorm:"not null;default:0;check:reserved_stock >= 0" binding:"gte=0" json:"reserved_stock"`
 
 	// Relationships
 	Product Product `gorm:"foreignKey:ProductID" json:"product,omitempty"`
